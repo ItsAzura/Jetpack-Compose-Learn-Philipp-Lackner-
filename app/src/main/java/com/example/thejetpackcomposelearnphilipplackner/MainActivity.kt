126fe9984ad9d1da6235b009570094f064d212c7
ItsAzura <azurawithluz@gmail.com>
2024-01-16 22:44:39 +0700

    change remove stuff

--- app/src/main/java/com/example/thejetpackcomposelearnphilipplackner/MainActivity.kt
+++ app/src/main/java/com/example/thejetpackcomposelearnphilipplackner/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.thejetpackcomposelearnphilipplackner
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
@@ -22,8 +28,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.example.thejetpackcomposelearnphilipplackner.ui.theme.TheJetpackComposeLearnPhilippLacknerTheme
+import androidx.compose.material3.AlertDialog
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,12 +41,19 @@ class MainActivity : ComponentActivity() {
                     mutableStateOf("")
                 }
                 var names by remember {
-                    mutableStateOf(listOf<String>("Quang","Azura"))
+                    mutableStateOf(listOf("Quang","Azura"))
                 }
                 var selectedIndexes by remember {
                     mutableStateOf(setOf<Int>())
                 }
-               Column(
+                var showDialog by remember {
+                    mutableStateOf(false)
+                }
+                var itemToDeleteIndex by remember {
+                    mutableStateOf(-1)
+                }
+
+                Column(
                    modifier = Modifier
                        .fillMaxSize()
                        .padding(6.dp)
@@ -63,19 +78,6 @@ class MainActivity : ComponentActivity() {
                            }
                        ) {
                          Text(text = "Add")
-                       }
-                       Spacer(modifier = Modifier.width(16.dp))
-                       Button(
-                           onClick = {
-                               if(selectedIndexes.isNotEmpty()){
-                                   names = names.filterIndexed{ index, _ ->
-                                       index !in selectedIndexes
-                                   }
-                                   selectedIndexes = emptySet()
-                               }
-                           }
-                       ) {
-                           Text(text = "Remove")
                        }
                    }
                    LazyColumn(){
@@ -102,11 +104,57 @@ class MainActivity : ComponentActivity() {
                                        }
                                    }
                                )
+                               Icon(
+                                   imageVector = Icons.Default.Delete,
+                                   contentDescription ="Remove",
+                                   modifier = Modifier
+                                       .padding(top = 10.dp)
+                                       .size(24.dp)
+                                       .clickable {
+                                       showDialog = true
+                                       itemToDeleteIndex = index
+                                   }
+                               )
 
                            }
                            Divider()
                        }
                    }
+                    if(showDialog){
+                        val context = LocalContext.current
+                        AlertDialog(
+                            onDismissRequest = {
+                                showDialog = false
+                                itemToDeleteIndex = -1
+                            },
+                            title = {
+                                Text(text = "Thông Báo")
+                            },
+                            text = {
+                                Text(text = "Bạn có muốn xoá item này không?")
+                            },
+                            confirmButton = {
+                                Button(onClick = {
+                                    names = names.toMutableList().apply {
+                                        removeAt(itemToDeleteIndex)
+                                    }
+                                    showDialog = false
+                                    itemToDeleteIndex = -1
+                                    Toast.makeText(context, "Item đã được xoá thành công", Toast.LENGTH_SHORT).show()
+                                }) {
+                                    Text(text = "Yes")
+                                }
+                            },
+                            dismissButton = {
+                                Button(onClick = {
+                                    showDialog = false
+                                    itemToDeleteIndex = -1
+                                }) {
+                                    Text(text = "No")
+                                }
+                            },
+                        )
+                    }
                }
                 
             }
